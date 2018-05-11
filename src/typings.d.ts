55efff5b97a159725f9bdafdc5c8503c0088ba3a
coder@360 <karthik.infotec99@gmail.com>
2018-05-11 18:42:43 +0530

    Update typings.d.ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,16 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
+$scope.sortingArrowClass = function(propertyName) {
+         
+                
+                 if($scope.propertyName == propertyName && !$scope.reverse)
+                     {
+                         return 'fa fa-caret-down';
+                     }
+                 else if($scope.propertyName == propertyName && $scope.reverse)
+                     {
+                        return 'fa fa-caret-up'; 
+                     }
+                 else if(!$scope.propertyName || $scope.propertyName != propertyName){
+                     return 'fa fa-sort'; 
+                 } 
+
+            };
